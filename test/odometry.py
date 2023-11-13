ca29cc4ec657f84daf5a1288829629b93596712b
Andreas Wangsanata <40526361+andreaswngsnt@users.noreply.github.com>
2023-11-12 20:19:21 -0800

    Update odometry.py

--- test/odometry.py
+++ test/odometry.py
@@ -1,5 +1,9 @@
+# NOTE: The robot is moving backwards
+
+import math
+import time
+from threading import (Event, Thread)
 from gpiozero import Motor
-from time import sleep
 import board
 import busio
 from adafruit_bno08x import (
@@ -9,37 +13,57 @@ from adafruit_bno08x.i2c import BNO08X_I2C
 
 # Setup
 baud_rate = 400000
-update_interval = 1 / 200000
+update_interval = 1 / 1000
 i2c = busio.I2C(board.SCL, board.SDA, frequency = baud_rate)
 bno = BNO08X_I2C(i2c)
 bno.enable_feature(BNO_REPORT_ACCELEROMETER)
 motorL = Motor(forward = 24, backward = 23)
 motorR = Motor(forward = 27, backward = 17)
 
+acceleration = 0
+speed = 0
+distance = 0
+
+def display_info():
+    while True:
+        print("Acceleration: %0.6f  m/s^2" % (acceleration))
+        print("Speed: %0.6f m/s" % (speed))
+        print("Distance: %0.6f m" % (distance))
+        time.sleep(0.5)
+        
+        if event.is_set():
+            break
+
 while True:
-    target_distance = input("Enter distance to travel:")
+    # Reset readings
+    acceleration = 0
+    speed = 0
+    distance = 0
+
+    target_distance = int(input("Enter distance to travel: "))
 
     if target_distance <= 0:
         continue
 
-    speed = 0
-    distance = 0
-    time_elapsed = 0
+    motorL.backward()
+    motorR.backward()
 
-    motorL.forward()
-    motorR.forward()
+    # Begin display thread
+    event = Event()
+    display_info_thread = Thread(target = display_info)
+    display_info_thread.start()
 
     while distance < target_distance:
-        sleep(update_interval)
         accel_x, accel_y, accel_z = bno.acceleration
-        speed += (accel_y * update_interval)
+        acceleration = -1 * accel_y
+        speed += (acceleration * update_interval)
         distance += (speed * update_interval)
-        time_elapsed += 1
+        
+        time.sleep(update_interval)
 
-        if time_elapsed % 100000 == 0:
-            print("Acceleration: %0.6f m/s^2" % (accel_y))
-            print("Speed: %0.6f m/s" % (speed))
-            print("Distance: %0.6f m" % (distance))
+    # Stop the thread
+    event.set()
+    display_info_thread.join()
 
     motorL.stop()
     motorR.stop()
