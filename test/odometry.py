d057a9685ce4ec3997108a2b7f876cde16ec1dac
Andreas Wangsanata <40526361+andreaswngsnt@users.noreply.github.com>
2023-11-21 21:54:35 -0800

    Refactor odometry & tune PID parameters

--- test/odometry.py
+++ test/odometry.py
@@ -10,68 +10,6 @@ from adafruit_bno08x import (
 )
 from adafruit_bno08x.i2c import BNO08X_I2C
 
-# Setup
-baud_rate = 400000
-update_interval = 1 / 100
-i2c = busio.I2C(board.SCL, board.SDA, frequency = baud_rate)
-bno = BNO08X_I2C(i2c)
-bno.enable_feature(BNO_REPORT_ACCELEROMETER)
-bno.enable_feature(BNO_REPORT_ROTATION_VECTOR)
-motorL = Motor(forward = 27, backward = 17)
-motorR = Motor(forward = 23, backward = 24)
-
-def euler_from_quaternion(x, y, z, w):
-    """
-    Convert a quaternion into euler angles (roll, pitch, yaw)
-    roll is rotation around x in degrees (counterclockwise)
-    pitch is rotation around y in degrees (counterclockwise)
-    yaw is rotation around z in degrees (counterclockwise)
-    """
-    t0 = +2.0 * (w * x + y * z)
-    t1 = +1.0 - 2.0 * (x * x + y * y)
-    roll_x = math.atan2(t0, t1)
-    
-    t2 = +2.0 * (w * y - z * x)
-    t2 = +1.0 if t2 > +1.0 else t2
-    t2 = -1.0 if t2 < -1.0 else t2
-    pitch_y = math.asin(t2)
-    
-    t3 = +2.0 * (w * z + x * y)
-    t4 = +1.0 - 2.0 * (y * y + z * z)
-    yaw_z = math.atan2(t3, t4)
-    
-    return roll_x * (180 / math.pi), pitch_y * (180 / math.pi), yaw_z * (180 / math.pi) # in degrees
-
-# Global variables
-accel_x = 0
-accel_y = 0
-accel_z = 0
-accel_x_offset = 0
-accel_y_offset = 0
-speed = 0
-distance = 0
-roll = 0
-pitch = 0
-yaw = 0
-reference_yaw = 0
-throttle_L = 0.75
-throttle_R = 0.75
-
-# Thread functions
-def display_info():
-    while True:
-        print("Acceleration: (%0.6f, %0.6f, %0.6f) m/s^2" % (accel_x - accel_x_offset, accel_y - accel_y_offset, accel_z))
-        print("Speed: %0.6f m/s" % (speed))
-        print("Distance: %0.6f m" % (distance))
-        print("Error: %0.6f" % (yaw - reference_yaw))
-        print("L: %0.6f R: %0.6f" % (throttle_L, throttle_R))
-        #print("Rotation: (%0.6f, %0.6f, %0.6f) degrees" % (roll, pitch, yaw))
-        time.sleep(0.5)
-        
-        if measuring_event.is_set():
-            break
-
-
 class PIDController:
     def __init__(self, k_p, k_i, k_d):
         self.k_p = k_p
@@ -90,14 +28,20 @@ class PIDController:
         self.__integrated_error = 0
         self.__prev_error = 0
 
+
 class Plant:
     def __init__(self):
         self.motor_L = Motor(forward = 27, backward = 17)
         self.motor_R = Motor(forward = 23, backward = 24)
 
+    def stop(self):
+        self.motor_L.stop()
+        self.motor_R.stop()
+
     def __del__(self):
         self.motor_L.stop()
         self.motor_R.stop()
+
 
 class Sensor:
     def __init__(self):
@@ -107,89 +51,145 @@ class Sensor:
         self.bno.enable_feature(BNO_REPORT_ACCELEROMETER)
         self.bno.enable_feature(BNO_REPORT_ROTATION_VECTOR)
 
+
 class ControlSystem:
     def __init__(self):
-        self.controller = PIDController(0.00005, 0, 1)
+        self.controller = PIDController(0.0001, 0, 0.0001)
         self.plant = Plant()
         self.sensor = Sensor()
+        self.update_interval = 1 / 100
+        self.accel_x = 0
+        self.accel_y = 0
+        self.accel_z = 0
+        self.accel_x_offset = 0
+        self.accel_y_offset = 0
+        self.speed = 0
+        self.distance = 0
+        self.roll = 0
+        self.pitch = 0
+        self.yaw = 0
+        self.reference_yaw = 0
+        self.throttle_L = 0.75
+        self.throttle_R = 0.75
+        self.measuring_event = None
 
-yaw_pid_controller = PIDController(0.00005, 0, 1)
-
-def correct_movement():
-    global throttle_L, throttle_R
-
-    while True:
+    def euler_from_quaternion(self, x, y, z, w):
         """
-        Error:
-        - Positive error means that the robot is veering left
-        - Negative error means that the robot is veering right
+        Convert a quaternion into euler angles (roll, pitch, yaw)
+        roll is rotation around x in degrees (counterclockwise)
+        pitch is rotation around y in degrees (counterclockwise)
+        yaw is rotation around z in degrees (counterclockwise)
         """
-        error = (360 + yaw - reference_yaw) if (reference_yaw >= 0 and yaw < 0) else (yaw - reference_yaw)
+        t0 = +2.0 * (w * x + y * z)
+        t1 = +1.0 - 2.0 * (x * x + y * y)
+        roll_x = math.atan2(t0, t1)
+        
+        t2 = +2.0 * (w * y - z * x)
+        t2 = +1.0 if t2 > +1.0 else t2
+        t2 = -1.0 if t2 < -1.0 else t2
+        pitch_y = math.asin(t2)
+        
+        t3 = +2.0 * (w * z + x * y)
+        t4 = +1.0 - 2.0 * (y * y + z * z)
+        yaw_z = math.atan2(t3, t4)
+        
+        return roll_x * (180 / math.pi), pitch_y * (180 / math.pi), yaw_z * (180 / math.pi) # in degrees
 
-        # Compensate the movement by varying the right throttle
-        corrective_throttle = yaw_pid_controller.get_output(error, update_interval)
-        if (throttle_L + corrective_throttle > 0.01) and (throttle_L + corrective_throttle < 0.99):
-            throttle_L += corrective_throttle
-            motorL.forward(throttle_L)
+    def display_info(self):
+        while True:
+            print("Acceleration: (%0.6f, %0.6f, %0.6f) m/s^2" % (self.accel_x - self.accel_x_offset, self.accel_y - self.accel_y_offset, self.accel_z))
+            print("Speed: %0.6f m/s" % (self.speed))
+            print("Distance: %0.6f m" % (self.distance))
+            print("Error: %0.6f" % (self.yaw - self.reference_yaw))
+            print("L: %0.6f R: %0.6f" % (self.throttle_L, self.throttle_R))
+            #print("Rotation: (%0.6f, %0.6f, %0.6f) degrees" % (roll, pitch, yaw))
+            time.sleep(0.5)
+            
+            if self.measuring_event.is_set():
+                break
 
-        if (throttle_R - corrective_throttle > 0.01) and (throttle_R - corrective_throttle < 0.99):
-            throttle_R -= corrective_throttle
-            motorR.forward(throttle_R)
+    def reset_measurements(self):
+        self.accel_x = 0
+        self.accel_y = 0
+        self.accel_z = 0
+        self.accel_x_offset = 0
+        self.accel_y_offset = 0
+        self.speed = 0
+        self.distance = 0
+        self.roll = 0
+        self.pitch = 0
+        self.yaw = 0
 
-        time.sleep(update_interval)
+    def correct_movement(self):
+        while True:
+            """
+            Error:
+            - Positive error means that the robot is veering left
+            - Negative error means that the robot is veering right
+            """
+            error = (360 + self.yaw - self.reference_yaw) if (self.reference_yaw >= 0 and self.yaw < 0) else (self.yaw - self.reference_yaw)
 
-        if measuring_event.is_set():
-            break
+            # Compensate the movement by varying the right throttle
+            corrective_throttle = self.controller.get_output(error, self.update_interval)
+            if (self.throttle_L + corrective_throttle > 0.01) and (self.throttle_L + corrective_throttle < 0.99):
+                self.throttle_L += corrective_throttle
+                self.plant.motor_L.forward(self.throttle_L)
+
+            if (self.throttle_R - corrective_throttle > 0.01) and (self.throttle_R - corrective_throttle < 0.99):
+                self.throttle_R -= corrective_throttle
+                self.plant.motor_R.forward(self.throttle_R)
+
+            time.sleep(self.update_interval)
+
+            if self.measuring_event.is_set():
+                break
+
+    def move(self, reference_distance):
+        self.reset_measurements()
+
+        # Begin display thread
+        self.measuring_event = Event()
+        display_info_thread = Thread(target = self.display_info)
+        display_info_thread.start()
+
+        # Get initial readings & offset
+        self.accel_x_offset = self.sensor.bno.acceleration[0]
+        self.accel_y_offset = self.sensor.bno.acceleration[1]
+        self.reference_yaw = self.euler_from_quaternion(*self.sensor.bno.quaternion)[2]
+
+        # Begin moving
+        self.plant.motor_L.forward(self.throttle_L)
+        self.plant.motor_R.forward(self.throttle_R)
+
+        # Begin movement correction thread
+        correct_movement_thread = Thread(target = self.correct_movement)
+        correct_movement_thread.start()
+
+        # Keep calculating distance until target distance reached
+        while self.distance < reference_distance:
+            self.accel_x, self.accel_y, self.accel_z = self.sensor.bno.acceleration
+            self.speed += ((self.accel_y - self.accel_y_offset)  * self.update_interval)
+            self.distance += (self.speed * self.update_interval)
+            self.roll, self.pitch, self.yaw = self.euler_from_quaternion(*self.sensor.bno.quaternion)
+
+            time.sleep(self.update_interval)
+
+        self.plant.stop()
+
+        # Stop movement correction thread
+        self.measuring_event.set()
+        self.correct_movement_thread.join()
+
+        # Stop display thread
+        display_info_thread.join()
+
+
+control_system = ControlSystem()
 
 while True:
-    # Reset readings
-    accel_x = 0
-    accel_y = 0
-    accel_z = 0
-    accel_x_offset = 0
-    accel_y_offset = 0
-    speed = 0
-    distance = 0
-
     target_distance = int(input("Enter distance to travel: "))
 
     if target_distance <= 0:
         continue
 
-    # Begin display thread
-    measuring_event = Event()
-    display_info_thread = Thread(target = display_info)
-    display_info_thread.start()
-
-    # Get initial readings & offset
-    accel_x_offset = bno.acceleration[0]
-    accel_y_offset = bno.acceleration[1]
-    reference_yaw = euler_from_quaternion(*bno.quaternion)[2]
-
-    # Start moving
-    motorL.forward(throttle_L)
-    motorR.forward(throttle_R)
-
-    # Begin movement correction thread
-    correct_movement_thread = Thread(target = correct_movement)
-    correct_movement_thread.start()
-
-    # Keep calculating distance until target distance reached
-    while distance < target_distance:
-        accel_x, accel_y, accel_z = bno.acceleration
-        speed += ((accel_y - accel_y_offset)  * update_interval)
-        distance += (speed * update_interval)
-        roll, pitch, yaw = euler_from_quaternion(*bno.quaternion)
-
-        time.sleep(update_interval)
-
-    # Stop movement correction thread
-    measuring_event.set()
-    correct_movement_thread.join()
-
-    # Stop moving
-    motorL.stop()
-    motorR.stop()
-
-    # Stop display thread
-    display_info_thread.join()
+    control_system.move(target_distance)
