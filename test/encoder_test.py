ab9843cd6f931f0729de9d2549a5bb6413634c26
Andreas Wangsanata <40526361+andreaswngsnt@users.noreply.github.com>
2024-01-19 14:02:26 -0800

    Update encoder_test.py

--- test/encoder_test.py
+++ test/encoder_test.py
@@ -9,13 +9,13 @@ class QuadratureEncoder(object):
         self._a_value = 0
         self._b_value = 0
 
-        encoder_a = DigitalInputDevice(pin_a)
-        encoder_a.when_activated = self._increment_a
-        encoder_a.when_deactivated = self._increment_a
+        self.encoder_a = DigitalInputDevice(pin_a)
+        self.encoder_a.when_activated = self._increment_a
+        self.encoder_a.when_deactivated = self._increment_a
 
-        encoder_b = DigitalInputDevice(pin_b)
-        encoder_b.when_activated = self._increment_b
-        encoder_b.when_deactivated = self._increment_b
+        self.encoder_b = DigitalInputDevice(pin_b)
+        self.encoder_b.when_activated = self._increment_b
+        self.encoder_b.when_deactivated = self._increment_b
         
     def reset(self):
         self._a_value = 0
@@ -30,6 +30,7 @@ class QuadratureEncoder(object):
     @property
     def a_value(self):
         return self._a_value
+    
     @property
     def b_value(self):
         return self._b_value
