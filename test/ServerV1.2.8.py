5d684029c76a0fc509cd31bd045df3b19f38aae5
Andreas Wangsanata <40526361+andreaswngsnt@users.noreply.github.com>
2024-03-04 12:32:14 -0800

    Limit the autonomous steering angle

--- test/ServerV1.2.8.py
+++ test/ServerV1.2.8.py
@@ -120,13 +120,28 @@ def camera_callback(output_frame, output_angle, server_socket_UDP):
         
 # Function for autonomous navigation
 def handle_autonomous_navigation():
+    # Run the code every 0.1 seconds
     while True:
+        # Run these if autonomous mode is detected
         if autonomous_mode:
+            # Start moving if the robot is not moving
             if control_system.throttle_L == 0 and control_system.throttle_R == 0:
                 control_system.move_forward()
 
+            # Adjust the steering angle, based on the lane detector
             if (control_system.throttle_L != 0 or control_system.throttle_R != 0) and angle is not None:
-                control_system.adjust_reference_yaw(angle - 90)
+                max_steering_angle = 10
+                input_steering_angle = angle - 90
+
+                # Limit the steering angle
+                if input_steering_angle > max_steering_angle:
+                    control_system.adjust_reference_yaw(max_steering_angle)
+                elif input_steering_angle < ((-1) * max_steering_angle):
+                    control_system.adjust_reference_yaw((-1) * max_steering_angle)
+                else:
+                    control_system.adjust_reference_yaw(input_steering_angle)
+                
+        # Stop the robot if autonomous mode is turned off
         else:
             control_system.stop()
 
